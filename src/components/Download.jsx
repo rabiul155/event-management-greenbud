b849bcee2b5f3b723f2b857659c49d91804fe866
rabiul155 <siyamict@gamil.com>
2024-04-06 16:31:10 +0600

    url change

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -9,6 +9,7 @@ function Download() {
         href="https://docs.google.com/forms/d/1kKfXOELAOcDdTTzc7sgvrPCuCjLcP5Is9Ur_t6sT76M/edit?ts=66111137"
         className="w-auto inline-flex justify-center items-center text-sm font-semibold shadow-lg transition duration-200 hover:bg-primaryDark hover:text-white text-secondary rounded-full border-2 border-secondary bg-white px-4 py-3"
         download
+        target="_blank"
       >
         Register Now
       </a>
